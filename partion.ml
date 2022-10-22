599790b81f1122ba6ba322ce3ef23c6afa3125a5
rayou <momoladebrouill@mcCHvkb4F9FwPxtPGSscRqxvbITiw7b0UmnNAtJq8Y0>
2022-10-22 18:54:53 +0200

    ça fonctionne comme je le souhaite, reste plus qu'à opti

--- partion.ml
+++ partion.ml
@@ -27,26 +27,45 @@ let rec partition_deux n a b =
 		let a',b' = (if b=n then (a+1,a+1) else (a,b+1)) in
 		v + (partition_deux n a' b')
 
+
+
 let rec fl l= 
 	match l with
-	| [] -> print_newline (); 1
+	| [] -> print_newline ();
 	| t::q -> print_int t; print_char ' '; fl q
 
+let goodfl l=
+	print_string "V - ";
+	fl l;
+	1
+
+let badfl l=
+	print_string "X   ";
+	fl l;
+	0
+
+let sum l = (List.fold_left ( + ) 0 l)
+let min a b = if a<b then a else b
 
 let partition (n:int) (q:int) =
-	let rec incre (l:int list) (prec:int) : (int list) =
+
+	let rec increment (l:int list) (prec:int) : (int list) =
 		match l with
 		| [] -> []
-		| [x] -> [x+prec]
-		| t::q -> (t+prec)::incre q (if t>List.hd q then 1 else 0) 
-	in		
+		| h::t ->
+			let coef =
+				if prec >= n then  
+					if h >= n then List.hd t else h+1
+		 		else h
+		 	in coef::(increment t coef)  
+	in
+			
 	let rec partion (l:int list) : int =
-		if List.hd (List.rev l) = n then 0
+		if List.hd (List.rev l) >= n then 0 (*dernier membre de la somme*)
 		else 
-			let v = (if n=(List.fold_left ( + ) 0 l) then fl l else 0 ) in
-			v + partion (incre l 1) 
+			(if sum l = n then goodfl l else badfl l) + partion (increment l (n+5)) 
 	in 
-		partion (List.init n (fun x->1))
+		partion (List.init q (fun _->1))
 				
 	
 	
