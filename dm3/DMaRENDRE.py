492fe2d5040857863b1860f7f583c2525ecf2192
momoladebrouill <momoladebrouill@gmail.com>
2023-03-20 18:36:03 +0100

    clean :rofl:

---
+++ dm3/DMaRENDRE.py
@@ -0,0 +1,7 @@
+def DMaRENDRE(n):
+    for i in range(n,1,-1):
+        print(i, "DM à rendre pour lundi")
+        print("prends en un, fais-le, ",i-1," DM à rendre pour lundi")
+    print("plus de DM à rendre pour lundi")
+    print("je retourne en cours,", n, "DM à rendre pour lundi")
+DMaRENDRE(5)
